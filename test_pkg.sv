433b9ed368f96e4e63dd8aa9dec1245ae70ba72d
Yasir Javed <yasir@renzym.com>
2026-06-28 00:21:05 +0500

    Made package files SV instead of SVH, created test_pkg and my_sequences_pkg

---
+++ test_pkg.sv
@@ -0,0 +1,11 @@
+package test_pkg;
+    import my_pkg::*;    
+    `include "uvm_macros.svh"
+    import uvm_pkg::*;
+    import my_sequences_pkg::*;
+
+    `include "my_test.svh"
+    `include "test1.svh"
+    `include "test2.svh"
+    `include "test3.svh"
+endpackage: test_pkg
