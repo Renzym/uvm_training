f90d7b0ce18f6e49f738778d10e5d8b2fcfd282d
Yasir Javed <yasir@renzym.com>
2026-06-28 00:45:01 +0500

    Improved packages and includes in 05

---
+++ 05 - Monitor and Subscriber/my_sequences_pkg.sv
@@ -0,0 +1,10 @@
+package my_sequences_pkg;
+`include "uvm_macros.svh"
+import uvm_pkg::*;
+
+import my_pkg::*;
+
+`include "read_modify_write.svh"
+`include "seq_of_commands.svh"
+
+endpackage: my_sequences_pkg
